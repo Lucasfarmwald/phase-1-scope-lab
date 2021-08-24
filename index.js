d0ea6b93811f47b9fb16a22450a2796631b73740
lucasfarmwald <farmwaldlucas@gmail.com>
2021-08-24 18:33:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your solution in this file!
+ var customerName = 'bob'
+ var bestCustomer = 
+
+function upperCaseCustomerName(x){
+      var x = customerName.toUpperCase
+
+}
+
+function setBestCustomer(a){
+      a = bestCustomer()
+}
